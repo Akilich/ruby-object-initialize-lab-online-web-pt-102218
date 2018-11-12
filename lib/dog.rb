24e44c6a26bd5a0d39b514d063c092ab406173b2
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-12 05:09:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
 class Dog
-  def initialize(name, breed = "Mutt")
+  def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
-   def name=(name)
+  def name=(name)
     @name = name
   end
-   def name
+  def breed=(breed)
+    @breed = breed
+  end
+  def name
     @name
   end
+  def breed
+    @breed
+  end
 end
-
-  
